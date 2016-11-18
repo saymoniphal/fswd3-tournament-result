a85589b011bdcf3c5289fc676af917bae4f0e5f5
Moniphal Say <moniphal@locationd.net>
2016-11-18 17:13:34 +0530

    add database configuration file

---
+++ config.py
@@ -0,0 +1,21 @@
+from configparser import ConfigParser
+
+def config(filename, section='postgresql'):
+   """Read database configuration file (similar to INI files in Windows).
+   Args:
+      - filename: the name of configuration file which follow format of:
+          [section_name]
+          key = value or key : value
+      - section: the name of the section in the file to fetch
+          (default to prosgresql) 
+
+   Returns:
+      dictionary object with key,value pair read from input file
+   """
+   parser = ConfigParser()
+   parser.read(filename)
+   if parser.has_section(section):
+       return parser.items()
+   else:
+       raise Exception('Section %s not found in file %s.',
+                       format(section,filename)) 
