c3a332d11ed95c05f1cc796bc5d8baee6d2caf6c
Moniphal Say <moniphal@locationd.net>
2016-11-19 19:45:02 +0530

    adding sql statement

--- tournament.sql
+++ tournament.sql
@@ -1,9 +1,40 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+CREATE TABLE player(
+   name text NOT NULL,
+   id SERIAL PRIMARY KEY
+);
 
+CREATE TABLE tournament(
+   name text NOT NULL,
+   year smallint check(
+       year between 0 and extract(year from current_date)
+   ), 
+   id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE match(
+   winner_id integer REFERENCES player (id),
+   loser_id integer REFERENCES player (id),
+   tournament_id integer REFERENCES tournament (id),
+   PRIMARY KEY (winner_id, loser_id, tournament_id)
+);
+
+-- Create a view as a result query of:
+-- all player and the number of matches the player has won (wins)
+-- and the number of matches the player has played (matches)
+-- in all tournaments 
+CREATE VIEW playerStandings_view AS
+   SELECT player.id, player.name, count (match.winner_id) as wins,
+          tournament.name as tournament
+   FROM player
+        LEFT JOIN match ON (player.id = match.winner_id)
+        LEFT JOIN tournament ON (match.tournament_id = tournament.id)
+   GROUP BY player.id, tournament.name ORDER BY wins;
+
+CREATE VIEW matches_view AS
+   SELECT player.id, match.winner_id, match.loser_id
+   FROM player
+        LEFT JOIN match ON (player.id = match.winner_id) OR
+                           (player.id = match.loser_id)
+        LEFT JOIN tournament ON (match.tournament_id = tournament.id)
