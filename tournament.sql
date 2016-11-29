7707a857e64368f7fcd84303406e96af870d3f98
Moniphal Say <moniphal@locationd.net>
2016-11-29 17:54:39 +0530

    drop table, view before creating

--- tournament.sql
+++ tournament.sql
@@ -1,13 +1,15 @@
 -- Table definitions for the tournament project.
-
+DROP TABLE IF EXISTS tournament CASCADE;
 CREATE TABLE tournament (
    name text NOT NULL,
    year smallint check(
        year between 0 and extract(year from current_date)
    ),
-   id SERIAL PRIMARY KEY
+   tournament_id SERIAL PRIMARY KEY
 );
 
+
+DROP TABLE IF EXISTS player CASCADE;
 CREATE TABLE player (
    name text NOT NULL,
    gender text,
@@ -15,20 +17,24 @@ CREATE TABLE player (
    id SERIAL PRIMARY KEY
 );
 
+
+DROP TABLE IF EXISTS tournamentplayers;
 CREATE TABLE tournamentplayers (
-   tournament_id integer REFERENCES tournament (id),
+   tournament_id integer REFERENCES tournament (tournament_id),
    player_id integer REFERENCES player (id),
    PRIMARY KEY (tournament_id, player_id)
 );
 
+
+DROP TABLE IF EXISTS match;
 CREATE TABLE match(
-   match_id SERIAL,
-   tournament_id integer REFERENCES tournament (id),
-   match_round integer NON NULL,
+   match_id SERIAL PRIMARY KEY,
+   tournament_id integer REFERENCES tournament (tournament_id),
+   match_round integer NOT NULL,
    loser_id integer REFERENCES player (id),
-   winner_id integer REFERENCES player (id),
-   PRIMARY KEY (match_id)
+   winner_id integer REFERENCES player (id)
 );
+
 
 -- Create a PL/pgSQL Procedure to ensure that in same match and tournament,
 -- the same player can not be a winner and a loser.
@@ -41,7 +47,7 @@ BEGIN
         WHERE (loser_id = NEW.loser_id AND winner_id = NEW.winner_id)
            OR (loser_id = NEW.winner_id AND winner_id = NEW.loser_id);
       IF result.exists <> 0 THEN
-        RAISE EXCEPTION 'duplicate match between % and % in same tournament',\
+        RAISE EXCEPTION 'duplicate match between % and % in same tournament',
                         NEW.loser_id, NEW.winner_id;
       END IF;
 
@@ -49,32 +55,39 @@ BEGIN
 END;
 $control_match$ LANGUAGE plpgsql;
 
+
 CREATE TRIGGER match_control BEFORE INSERT OR UPDATE on match
   FOR EACH ROW EXECUTE PROCEDURE control_match();
 
 -- Create a view to get:
 -- all player and the number of matches player has won (wins)
 -- in all tournaments
-CREATE VIEW playerwin_view AS
+DROP VIEW IF EXISTS win_view;
+CREATE VIEW win_view AS
    SELECT player.id, player.name, COUNT (match.winner_id) as wins,
-          tournament.name as tournament
+          tournament.tournament_id
    FROM player
         LEFT JOIN match ON (player.id = match.winner_id)
-        LEFT JOIN tournament ON (match.tournament_id = tournament.id)
-   GROUP BY player.id, tournament.name ORDER BY wins;
+        LEFT JOIN tournament ON (match.tournament_id = tournament.tournament_id)
+   GROUP BY player.id, tournament.tournament_id ORDER BY wins;
+
 
 -- Create a view to get:
 -- all player and the number of matches the player has played (matches)
 -- in all tournaments
+DROP VIEW IF EXISTS match_view;
 CREATE VIEW match_view AS
    SELECT player.id, player.name, COUNT (match.match_id) as matches,
-          tournament.name as tournament
+          tournament.tournament_id
    FROM player
         LEFT JOIN match ON (player.id = match.winner_id) OR (player.id = match.loser_id)
-        LEFT JOIN tournament ON (match.tournament_id = tournament.id)
-   GROUP BY player.id, tournament.name ORDER BY matches;
+        LEFT JOIN tournament ON (match.tournament_id = tournament.tournament_id)
+   GROUP BY player.id, tournament.tournament_id ORDER BY matches;
 
+
+DROP VIEW IF EXISTS playerStandings_view;
 CREATE VIEW playerStandings_view AS
-   SELECT playerwin_view.tournament, playerwin_view.id, playerwin_view.name,
-          match_view.matches, playerwin_view.wins from playerwin_view
-   JOIN match_view on (playerwin_view.id = match_view.id) ORDER BY wins;
+   SELECT win_view.id, win_view.name, win_view.wins,
+          match_view.matches
+   FROM win_view
+   JOIN match_view on (win_view.id = match_view.id) ORDER BY wins;
